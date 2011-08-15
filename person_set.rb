d3178b29245419e5799bdc832a1407df70a319ba
Jared Beck <jared@jaredbeck.com>
2011-08-15 03:00:12 -0400

    setting the bar

--- person_set.rb
+++ person_set.rb
@@ -26,6 +26,12 @@ class PersonSet
     return ps
   end
 
+  def self.print_person_array(a)
+    a.each() do |p|
+      puts p.name + "\t\t" + p.rating.to_s
+    end
+  end
+
   def each() @people.each { |p| yield p } end
   def initialize() @people = [] end
   def push(p) @people.push p end
--- smpair.rb
+++ smpair.rb
@@ -1,18 +1,29 @@
 require './person_set'
 
 # cmd line args
-if ARGV.length != 1 then
-  puts "Usage: ruby smpair.rb players-file"
+if ARGV.length != 2 then
+  puts "Usage: ruby smpair.rb players-file bar"
+  puts " players-file\tpath to a csv file with headers (eg. name,rating)"
+  puts " bar\t\tthe rating threshold defining the top group"
   exit
 end
 
+player_list_filepath = ARGV[0]
+bar = ARGV[1]
+
+# Bar must be a decimal number
+if !bar.match(/^\-?[0-9]+\.[0-9]+$/) or bar.to_f == 0.0 then
+  puts "Invalid bar.  Bar must be a decimal number.  See usage instructions."
+  exit
+end
+bar = bar.to_f
+
 # read csv player list
-ps = PersonSet.read_csv(ARGV[0])
+ps = PersonSet.read_csv player_list_filepath
 
 # get array of people, ordered by descending rating
 person_array = ps.sort.reverse
 
-# print players
-person_array.each() do |p|
-  puts p.name + "\t\t" + p.rating.to_s
-end
+# get players above the bar, ordered by descending rating
+people_above_bar = ps.select { |p| p.rating > bar }.sort.reverse
+PersonSet.print_person_array people_above_bar
