592765e0e648f7d63a55d8f07e3e4cd6a86016a2
Jared Beck <jared@jaredbeck.com>
2011-08-15 02:59:52 -0400

    comment

--- person.rb
+++ person.rb
@@ -19,8 +19,9 @@ class Person
 
   def valid_rating?
     # In the AGA, valid kyu ratings are -1.0 and below, while valid dan 
-    # ratings are 1.0 and above. There is no upper or lower limit in the 
-    # AGA database. The current range is -49.67667 to 10.24712
+    # ratings are 1.0 and above (including pros). There is no upper or
+    # lower limit in the AGA database. The current range is -49.67667 
+    # to 10.24712
     !@rating.nil? and (@rating <= -1.0 or @rating >= +1.0)
   end
 end
