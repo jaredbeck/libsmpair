00d2b7d0a9fce505f696299e2651c877dd91855d
Jared Beck <jared@jaredbeck.com>
2011-08-15 01:58:30 -0400

    make use of new sorting feature

--- smpair.rb
+++ smpair.rb
@@ -3,13 +3,16 @@ require './person_set'
 # cmd line args
 if ARGV.length != 1 then
   puts "Usage: ruby smpair.rb players-file"
+  exit
 end
 
 # read csv player list
-ps = PersonSet.new()
-ps.read_csv ARGV[0]
+ps = PersonSet.read_csv(ARGV[0])
+
+# get array of people, ordered by descending rating
+person_array = ps.sort.reverse
 
 # print players
-ps.to_array.each() do |p|
+person_array.each() do |p|
   puts p.name + "\t\t" + p.rating.to_s
 end
