eeb18379958c743f5caac7daf0c698b6093a80b7
Jared Beck <jared@jaredbeck.com>
2012-01-01 02:53:52 -0500

    put rspec options in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,12 @@
 require 'smpair'
+
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color_enabled = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation
+end
